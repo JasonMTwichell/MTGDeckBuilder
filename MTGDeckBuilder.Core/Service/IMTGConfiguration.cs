c2f2c39a4d81d122997643b7a03d253530c524ec
Qumbaala <jasonmtwichell@gmail.com>
2022-08-26 23:33:21 -0400

    Added basic config implementation and docker orch

---
+++ MTGDeckBuilder.Core/Service/IMTGConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTGDeckBuilder.Core.Service
+{
+    public interface IMTGConfiguration
+    {
+        string GetConfigurationValue(string key);
+    }
+}
